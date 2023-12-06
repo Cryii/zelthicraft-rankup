ee73324808fdff9581fd1b9d389b446130129da8
spozap <spozapdev@gmail.com>
2023-12-06 13:49:06 +0100

    initial commit

---
+++ src/main/java/dev/spozap/zelthicraftrankup/Main.kt
@@ -0,0 +1,13 @@
+package dev.spozap.zelthicraftrankup
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        println("Se ha iniciado el plugin cacaota")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
