57131562f8e22c1169196554d5033807461a546f
spozap <spozapdev@gmail.com>
2023-12-06 14:14:43 +0100

    feat: lp hook

--- src/main/java/dev/spozap/zelthicraftrankup/Main.kt
+++ src/main/java/dev/spozap/zelthicraftrankup/Main.kt
@@ -1,10 +1,22 @@
 package dev.spozap.zelthicraftrankup
 
+import net.luckperms.api.LuckPerms
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
+
+    private lateinit var luckPermsAPI : LuckPerms
+
     override fun onEnable() {
         println("Se ha iniciado el plugin cacaota")
+        val luckPermsProvider = Bukkit.getServicesManager().getRegistration(LuckPerms::class.java)
+        if (luckPermsProvider == null) {
+            this.pluginLoader.disablePlugin(this)
+            return
+        }
+
+        luckPermsAPI = luckPermsProvider.provider
     }
 
     override fun onDisable() {
